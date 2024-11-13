d32417d7616dd97005d74c3e200dad33feb2c9e4
Abhay <abhay@cybersify.tech>
2024-11-13 19:52:24 +0530

    code push

--- src/component/Sent_Purposal.tsx
+++ src/component/Sent_Purposal.tsx
@@ -87,7 +87,7 @@ const Sent_Purposal = ({ data1 }: any) => {
   const pearlsLeadId = searchParams.get("pearls_lead_id");
   const userId = searchParams.get("user_id");
   const field_for: any = searchParams.get("field_for");
-  const field_Type: any = searchParams.get("fieldType");
+
   // Log the pearlsLeadId to the console
   console.log(pearlsLeadId, "searchParam");
   console.log(userId, "userId");
@@ -176,7 +176,7 @@ const Sent_Purposal = ({ data1 }: any) => {
       reader.readAsDataURL(imageFile);
     }
   };
-  const [activeKey, setActiveKey] = useState<any>(field_Type);
+  const [activeKey, setActiveKey] = useState<any>("email");
 
   const onFinish = async (values: any) => {
     console.log("Form submitted with values:", file, values);
@@ -299,6 +299,7 @@ const Sent_Purposal = ({ data1 }: any) => {
   };
   const handleClearLocalStorage = () => {
     localStorage.removeItem("formValues");
+    router.back()
   };
   const entriesArray = Object.entries(formValues);
   console.log(entriesArray, "formValues");
@@ -340,7 +341,7 @@ const Sent_Purposal = ({ data1 }: any) => {
           </Link>
           <h2>Send </h2>
           <Tabs
-            defaultActiveKey={field_Type}
+            defaultActiveKey="all"
             items={tabs}
             onChange={handleChange}
             style={{
@@ -350,7 +351,7 @@ const Sent_Purposal = ({ data1 }: any) => {
             }}
           />
           <Card
-            title={`${capFirst(activeKey || field_Type)} Form`}
+            title={`${capFirst(activeKey || "Email")} Form`}
             extra={
               <Button type="primary" onClick={showModal}>
                 Preview
